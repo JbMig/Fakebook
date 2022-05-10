3ce9cb616baf103d949b90feb34d4e3d62983673
JbMig <jbmigone@gmail.com>
2022-05-10 10:49:33 +0200

    fix: incoming changes

--- php_partial/timeline.php
+++ php_partial/timeline.php
@@ -2,11 +2,7 @@
 
 // start buffering
 ob_start();
-<<<<<<< HEAD
-$title = "Fakebook - fil d'acualité";
-=======
 $title = "Fakebook - fil d'actualité";
->>>>>>> main
 
 require_once __DIR__ . "/../html_partial/timeline.php";
 // clean buffering in $content
