677c418ecb01fd23844b587b2f7691db5c4f77c2
Doud75 <adrienquimbre@gmail.com>
2022-05-10 10:38:37 +0200

    fix: orthographe in

--- php_partial/timeline.php
+++ php_partial/timeline.php
@@ -2,7 +2,7 @@
 
 // start buffering
 ob_start();
-$title = "Fakebook - fil d'acualité";
+$title = "Fakebook - fil d'actualité";
 
 require_once __DIR__ . "/../html_partial/timeline.php";
 // clean buffering in $content
