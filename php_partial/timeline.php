cf6c28ee67ca94229680014a65b2698bcca9cff3
Doud75 <adrienquimbre@gmail.com>
2022-05-10 15:34:04 +0200

    fix:

--- php_partial/timeline.php
+++ php_partial/timeline.php
@@ -3,7 +3,6 @@
 // start buffering
 ob_start();
 $title = "Fakebook - fil d'actualité";
-$_SESSION["users"]["user_id"] = 1;
 
 require_once __DIR__ . "/../html_partial/timeline.php";
 // clean buffering in $content
