a2274fb39d9777d652c1320ac245c089a93aa185
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-05-11 15:23:20 +0200

    fix : friend request added to the database

--- php_partial/friend_request.php
+++ php_partial/friend_request.php
@@ -4,12 +4,12 @@ $user_id = $_SESSION["user"]["user_id"];
 if($_SERVER["REQUEST_METHOD"] === "POST") {
     if(isset($_POST["friend_request"])) {
         require_once __DIR__ . "/../database/pdo.php";
-        $profile_id = filter_input(INPUT_POST, '$profile_id');
+        $profile_id = filter_input(INPUT_POST, 'friend_request');
 		$maRequete = $pdo->prepare(
 			"INSERT INTO `relationships` (`user_id_a`, `user_id_b`)
 			VALUES(:userId, :profileId)");
 			$maRequete->execute([
-				":userId" => $user_id
+				":userId" => $user_id,
 				":profileId" => $profile_id
 			]);
 		http_response_code(302);
