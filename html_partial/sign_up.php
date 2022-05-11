a32c4ee48e1ad76c4e47484453f22faa18fc5574
Doud75 <adrienquimbre@gmail.com>
2022-05-11 21:17:15 +0200

    fix: remove main and body in signup

--- html_partial/sign_up.php
+++ html_partial/sign_up.php
@@ -1,20 +1,16 @@
-<body class="bodyLogin">
-    <main class="loginMenu">
-    <div class="container col-md-3 d-grid gap-2  border border-primary rounded" style="margin-top : 10%">
-                <form method="POST">
-                <span class="col-md-6 mx-2 ">First Name :</span>
-                <span class="col-md-6 mx-5 px-3 ">Last Name :</span>
-                <input class="col-md-5 mx-1 my-3" type="text" id="firstName" name="firstName" placeholder="">
-                <input class="col-md-5 mx-1" type="text" id="lastName" name="lastName" placeholder="">
-                <span class="col-md-12 my-3">Email :</span>
-                <input class="col-md-12 my-3" type="email" id="email" name="email" placeholder="">
-                <span class="col-md-12 my-3">Password :</span>
-                <input class="col-md-12 my-3" type="password" id="password" name="password" placeholder="">
-                <span class="col-md-12 my-3">Confirm password :</span>
-                <input class="col-md-12 my-3" type="password" id="confirmPassword" name="confirmPassword" placeholder="">
-                <input class="col-md-12 my-3 bg-primary" type="submit" id="valider" value="Valider"/>
-                </form>
-            <a id="inscrire" href="/login">Login</a>
-    </div>
-    </main>
-</body>
+<div class="container col-md-3 d-grid gap-2  border border-primary rounded" style="margin-top : 10%">
+    <form method="POST">
+        <span class="col-md-6 mx-2 ">First Name :</span>
+        <span class="col-md-6 mx-5 px-3 ">Last Name :</span>
+        <input class="col-md-5 mx-1 my-3" type="text" id="firstName" name="firstName" placeholder="">
+        <input class="col-md-5 mx-1" type="text" id="lastName" name="lastName" placeholder="">
+        <span class="col-md-12 my-3">Email :</span>
+        <input class="col-md-12 my-3" type="email" id="email" name="email" placeholder="">
+        <span class="col-md-12 my-3">Password :</span>
+        <input class="col-md-12 my-3" type="password" id="password" name="password" placeholder="">
+        <span class="col-md-12 my-3">Confirm password :</span>
+        <input class="col-md-12 my-3" type="password" id="confirmPassword" name="confirmPassword" placeholder="">
+        <input class="col-md-12 my-3 bg-primary" type="submit" id="valider" value="Valider"/>
+    </form>
+    <a id="inscrire" href="/login">Login</a>
+</div>
