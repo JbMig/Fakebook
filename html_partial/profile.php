87d8ec4f7b64141bac895413cc51e1b1f23c378d
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-05-11 11:42:22 +0200

    feat : profile page

--- html_partial/profile.php
+++ html_partial/profile.php
@@ -54,6 +54,36 @@
 		</div>
 		<div>
 		<!-- past articles -->
+			<?php foreach ($articles as $article): ?>
+				<div id="article" style="margin-top:20px; border: solid 1px black; padding: 10px; width: 500px">
+					<form id="goToProfile" action="/profile" method="post">
+						<input type="hidden" name="profil_id" value="<?= $article["user_id"] ?>" />
+						<button type="submit" id="profil_picture" style="background: white; border:0; padding:5px;">
+							<img src="img_profil/<?= $profile["profil_picture"] ?>" alt="" width="40px">
+						</button>
+						<button type="submit" id="first_name" style="background: white; border:0; padding:0;"> 
+							<?= $profile["first_name"] . " " . $profile["last_name"] ?> 
+						</button>
+					</form>
+					<span id="date"><?= $article["date"] ?></span>
+					<br>
+					<span id="data"><?= $article["content"] ?></span>
+					<br>
+					<?php if($article["picture"]) :?>
+						<img id="image_article" width="300px" src="img_post/<?=$article["picture"]?>" >
+					<?php endif; ?>
+					<?php if($article["user_id"] === $_SESSION["user"]["user_id"]) :?>
+						<form id="delete_article" method="post" action="/delete_article">
+							<button type="submit" id="delete_btn">Supprimer</button>
+							<input type="hidden" name="article_id" value="<?=$article["article_id"]?>">
+							<input type="hidden" name="article_user" value="<?=$article["user_id"]?>">
+						</form>
+					<?php endif ?>
+
+				</div>
+			<?php endforeach;?>
+
+
 		</div>
 	</div>
 	<div>
--- php_partial/profile.php
+++ php_partial/profile.php
@@ -1,10 +1,40 @@
 <?php
 ob_start();
-$title = "Fakebook - Profil de ...";
-
-require_once __DIR__ . "/../database/pdo.php"; // accessing the database
-
 $user_id = $_SESSION["user"]["user_id"]; // needed to check whether it's the user's page or someone else's.
+
+if($_SERVER["REQUEST_METHOD"] === "GET") {
+	$title = "Fakebook - Profil de " . $user_id;
+	
+}
+
+
+
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+	require_once __DIR__ . "/../database/pdo.php";  // accessing the database
+	$profile_id = filter_input(INPUT_POST, "profil_id");
+
+    $maRequete = $pdo->prepare("SELECT `user_id`, `email`, `password`, `first_name`, `last_name`, `profil_picture`, `banner`, `status` FROM `users` WHERE `user_id` = :profile_id;");
+        $maRequete->execute([
+            ":profile_id" => $profile_id
+        ]);
+	$profile = $maRequete->fetch();
+
+	$title = "Fakebook - Profil de " . $profile["first_name"] . " " . $profile["last_name"];
+
+
+
+	$maRequete = $pdo->prepare("SELECT * FROM `articles` WHERE `user_id`=:profile_id ORDER BY `date` DESC"); // add condition for relationship
+		$maRequete->execute([
+			":profile_id" => $profile_id
+		]);
+		$articles = $maRequete->fetchAll(PDO::FETCH_ASSOC);
+
+
+}
+
+
+
 
 
 
