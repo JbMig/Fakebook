25da9c168eb2c0c7fbe60f5d9f995ba8247cf5cb
JbMig <jbmigone@gmail.com>
2022-05-12 14:44:11 +0200

    xd

--- php_partial/get_style.php
+++ php_partial/get_style.php
@@ -1,7 +1,5 @@
-<?php if(isset($_SESSION)["user"]['user_id']) && $_SESSION["user"]['theme'] === 1){?>
-        <link rel="stylesheet" type="text/css" href="style/style.css?<?php echo time();?>">'
-        <link rel="stylesheet" type="text/css" href="style/darkmode.css?<?php echo time();?>">'
-    <?php } else { ?>
-        <link rel="stylesheet" type="text/css" href="style/style.css?<?php echo time();?>">'
-        <link rel="stylesheet" type="text/css" href="style/lightmode.css?<?php echo time();?>">'
-    <?php } ?>
+<?php if(isset($_SESSION["user"]['user_id']) && $_SESSION["user"]['theme'] === 1) {?>
+    <link rel="stylesheet" type="text/css" href="/style/darkmode.css?<?php echo time();?>">
+<?php } else { ?>
+    <link rel="stylesheet" type="text/css" href="/style/lightmode.css?<?php echo time();?>">
+<?php } ?>
