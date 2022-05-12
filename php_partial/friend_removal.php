54885b89a2649d2c2f989e1c6a721fccfeb04305
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-05-12 11:34:19 +0200

    refactor: friend removal (working !)

--- php_partial/friend_removal.php
+++ php_partial/friend_removal.php
@@ -1,25 +1,21 @@
 <?php
     if($_SERVER["REQUEST_METHOD"] === "POST") {
-        if(isset($_POST["article_id"])) {
+        if(isset($_POST["friend_removal"])) {
             // get info from form
             require "../database/pdo.php";
-            $profile_id = filter_input(INPUT_POST, "profile_id");
+            $profile_id = filter_input(INPUT_POST, "friend_removal");
             $user_id = $_SESSION["user"]["user_id"];
-            // if user_id = article_id, i can delete the article
-
-			// delete relatioship from the database
-			$maRequete = $pdo->prepare("DELETE FROM `relationships` WHERE (`user_id_a` = :profile_id AND `user_id_b` = :userId) OR (`user_id_b` = :profile_id AND `user_id_a` = :userId)");
+			// delete relatioship from database
+			$maRequete = $pdo->prepare("DELETE FROM `relationships` WHERE (`user_id_a` = :profile_id AND `user_id_b` = :userId) OR (`user_id_b` = :profile_id AND `user_id_a` = :userId);");
 			$maRequete->execute([
 				":profile_id" => $profile_id,
 				":userId" => $user_id
 			]);
-			// go to last location
+			// go back to profile
 			http_response_code(302);
 
 			header("Location: /profile");
-
 			exit();
-
-        }
+		}
     }
 ?>
