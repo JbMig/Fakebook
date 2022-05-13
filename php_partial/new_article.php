5d5238ccdf9af05737b118665e8b6a3362e6d04e
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-05-13 16:23:50 +0200

    refactor: trying to add new article to stats

--- php_partial/new_article.php
+++ php_partial/new_article.php
@@ -7,6 +7,7 @@
             if($_FILES["fileToUpload"]["name"]) {
                 require_once __DIR__ . "/upload_img_post.php";
             } else {
+				// 1st we create the article in the articles table
                 $maRequete = $pdo->prepare(
                     "INSERT INTO `articles` (`content`, `user_id`)
                     VALUES(:content, :userId)");
@@ -14,6 +15,25 @@
                         ":content" => $text,
                         ":userId" => $user_id
                     ]);
+				// then we adjust the writer's stats
+				$maRequete = $pdo->prepare(
+                    "SELECT `user_id`, `nb_article` FROM `stats`
+					WHERE `user_id` = :userId;");
+                    $maRequete->execute([
+                        ":userId" => $user_id
+                    ]);
+				$nb_art = $maRequete->fetch();
+				$new_nb_art = $nb_art + 1;
+
+				$maRequete = $pdo->prepare(
+                    "UPDATE `stats`
+					SET `nb_article` = :new_nb_art
+					WHERE `user_id` = :userId;");
+                    $maRequete->execute([
+                        ":userId" => $user_id,
+                        ":new_nb_art" => $new_nb_art
+                    ]);
+					
                 http_response_code(302);
                 $direction = explode("/",$_SERVER["HTTP_REFERER"]);
                 if($direction[3] === "profile") {
