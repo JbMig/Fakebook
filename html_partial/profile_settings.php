b960b05754be63cf6959526ea17675e61b3d7d29
JbMig <jbmigone@gmail.com>
2022-05-13 14:16:06 +0200

    theme

--- html_partial/profile_settings.php
+++ html_partial/profile_settings.php
@@ -1,17 +1,7 @@
 <h2>Sélection du thème</h2>
-<form method = "post">
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="choice" id="light" value="0">
-        <label class="form-check-label" for="light">
-            Mode jour
-        </label>
-    </div>
-    <div class="form-check">
-        <input class="form-check-input" type="radio" name="choice" id="dark" value="1" checked>
-        <label class="form-check-label" for="dark">
-            Mode nuit
-        </label>
-    </div>
-    <input type="checkbox" class="btn-check" id="btn-check" autocomplete="off">
-    <label class="btn btn-primary" for="btn-check">Valider</label>
+
+<form method = "post" action = "/theme">
+    <input class="form-check-input" type="hidden" name="choice" id="theme_input" value="<?= $theme ?>">
+    <button class="btn btn-primary" type="submit">Changer theme</label>
 </form>
+
