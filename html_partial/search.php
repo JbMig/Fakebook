d974c6484171ffc8c578da0ff9f9b7e8975cca99
lightvic <v_guillerd@hetic.eu>
2022-05-13 15:40:36 +0200

    fix : search bar better result

--- html_partial/search.php
+++ html_partial/search.php
@@ -1,16 +1,19 @@
 <?php
 $memory=[];
+$end_search=[];
 foreach ($profiles as $profile){
+    $tampon_first = $who_first_name;
+    $tampon_last = $who_last_name;
     while(strlen($who_first_name) > 0 || strlen($who_last_name)>0){
-        $first_name = $profile["first_name"];
-        $last_name = $profile["last_name"];
+        $first_name = strtolower($profile["first_name"]);
+        $last_name = strtolower($profile["last_name"]);
         $user_id =$profile["user_id"];
         if (strlen($who_first_name) > 0){
             if(in_array($user_id,$memory) == false){
             array_push($memory,$user_id);?>
             <form id="goToProfile" action="/profile" method="post">
                 <button type="submit" id="first_name" style="background: white; border:0; padding:0;"> 
-                    <?= $first_name . " " . $last_name?> 
+                    <?= $first_name . " " . $last_name . " ". $user_id?> 
                     
                 </button>
             </form>
@@ -33,5 +36,6 @@ foreach ($profiles as $profile){
         }
 
     };
+    $who_first_name = $tampon_first;
+    $who_last_name = $tampon_last;
 }
-?>
--- php_partial/search.php
+++ php_partial/search.php
@@ -6,10 +6,11 @@ $who = filter_input(INPUT_POST,"search");
 $who_name = explode(" ",$who);
 $who_first_name = $who_name[0];
 $who_last_name = $who_name[1];
-$maRequete = $pdo->prepare("SELECT `user_id` ,`first_name`, `last_name` FROM `users` HAVING (`first_name` = :who_first_name) AND (`last_name` = :who_last_name);");
+$maRequete = $pdo->prepare('SELECT `user_id` ,`first_name`, `last_name` FROM `users` WHERE ((`first_name` LIKE :who_first_name) AND (`last_name` LIKE :who_last_name));');
 $maRequete->execute([
-    ":who_last_name" => $who_last_name,
-    ":who_first_name" => $who_first_name
+    // ":who_last_name" => $who_last_name,
+    ":who_last_name" => "%".$who_last_name."%",
+    ":who_first_name" => "%".$who_first_name."%"
 ]);
 $profiles = $maRequete->fetchAll(PDO::FETCH_ASSOC);
 require_once __DIR__ . "/../html_partial/search.php";
