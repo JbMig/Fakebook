634502d9ef2fa6740f36ec4c0c36c66f4c8c0684
Doud75 <adrienquimbre@gmail.com>
2022-05-13 11:02:59 +0200

    feat: we can modify our comment

--- router.php
+++ router.php
@@ -58,6 +58,9 @@ switch ($uri) {
     case "/delete_comment":
         require_once __DIR__ . "/php_partial/delete_comment.php";
         break;
+    case "/modify_comment":
+        require_once __DIR__ . "/php_partial/modify_comment.php";
+        break;
 }
 
 // we require base_html to display $content ($content references in files from php_partial)
