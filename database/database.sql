bda25f0011179b387dbdce8e01c7dc58e2fb46f3
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-05-14 12:25:55 +0200

    style: ordering the db

--- database/database.sql
+++ database/database.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`profil_picture` VARCHAR(255) DEFAULT "default_profile_pic.png",
 	`banner` VARCHAR(255) DEFAULT "default_banner.jpg",
 	`status` ENUM('active', 'inactive') DEFAULT 'active',
+	`theme` TINYINT DEFAULT 0,
 	PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB;
 
@@ -37,6 +38,7 @@ CREATE TABLE IF NOT EXISTS `articles` (
 	`content` TEXT,
 	`date` DATETIME DEFAULT CURRENT_TIMESTAMP,
 	`picture` VARCHAR(255),
+	`like_count` INT DEFAULT 0,
 	`user_id` INT NOT NULL,
 	`group_id` INT,
 	`page_id` INT,
@@ -48,6 +50,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
 	`content` TEXT NOT NULL,
 	`picture` VARCHAR(255),
 	`date` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	`like_count` INT DEFAULT 0,
 	`article_id` INT NOT NULL,
 	`user_id` INT NOT NULL,
 	PRIMARY KEY (`comment_id`)
@@ -56,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
 CREATE TABLE IF NOT EXISTS `likes` (
 	`like_id` INT NOT NULL AUTO_INCREMENT,
 	`date` DATETIME DEFAULT CURRENT_TIMESTAMP, -- needed to date the notifications
-	`article_id` INT NOT NULL,
+	`article_id` INT,
 	`comment_id` INT,
 	`user_id` INT NOT NULL,
 	PRIMARY KEY (`like_id`)
@@ -86,6 +89,8 @@ CREATE TABLE IF NOT EXISTS `followers` (
 
 CREATE TABLE IF NOT EXISTS `relationships` (
 	`relation_id` INT NOT NULL AUTO_INCREMENT,
+	`status` ENUM('pending','approved') DEFAULT 'pending',
+	`blocked` ENUM('yes', 'no') DEFAULT 'no',
 	`user_id_a` INT NOT NULL,
 	`user_id_b` INT NOT NULL,
 	PRIMARY KEY (`relation_id`)
@@ -126,6 +131,19 @@ CREATE TABLE IF NOT EXISTS `notifications` (
 	`comment_id` INT,
 	`article_id` INT,
 	PRIMARY KEY (`notif_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `stats` (
+	`stat_id` INT NOT NULL AUTO_INCREMENT,
+	`nb_articles` INT DEFAULT 0,
+	`nb_comments` INT DEFAULT 0,
+	`nb_likes` INT DEFAULT 0,
+	`likes_on_articles` INT DEFAULT 0,
+	`likes_on_comments` INT DEFAULT 0,
+	`comments_on_articles` INT DEFAULT 0,
+	`nb_friends` INT DEFAULT 0,
+	`user_id` INT NOT NULL,
+	PRIMARY KEY (`stat_id`)
 ) ENGINE=InnoDB;
 
 
@@ -176,22 +194,13 @@ ADD CONSTRAINT FOREIGN KEY (`comment_id`) REFERENCES `comments`(`comment_id`) ON
 ADD CONSTRAINT FOREIGN KEY (`page_id`) REFERENCES `pages`(`page_id`) ON DELETE CASCADE,
 ADD CONSTRAINT FOREIGN KEY (`group_id`) REFERENCES `groups`(`group_id`) ON DELETE CASCADE;
 
-ALTER TABLE `users`
-ADD `theme` TINYINT DEFAULT 0;
+ALTER TABLE `stats`
+ADD CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE;
+ 
 
-ALTER TABLE `relationships`
-ADD `status` ENUM('pending','approved') DEFAULT 'pending' AFTER `relation_id`;
 
-ALTER TABLE `relationships`
-ADD `blocked` ENUM('yes', 'no') DEFAULT 'no' AFTER `status`;
 
-ALTER TABLE `articles`
-ADD `like_count` INT DEFAULT 0 AFTER `picture`;
 
-ALTER TABLE `comments`
-ADD `like_count` INT DEFAULT 0 AFTER `date`;
-
-ALTER TABLE `likes` MODIFY `article_id` INT;
 
 
 
@@ -205,7 +214,7 @@ ALTER TABLE `likes` MODIFY `article_id` INT;
 
 
 
--- Fake users and articles
+-- Fake users and articles, so that the social network isn't completely empty at first (may be useless. we'll see.)
 INSERT INTO `users` (`email`, `password`, `first_name`, `last_name`)
 VALUES
 	("toto@gmail.com", "toto", "Thomas", "Tortue"),
@@ -226,47 +235,22 @@ VALUES
 	("Lire le texte et répondre aux questions page 132. Suivez, les gars...", 1, 4, '2022-04-08 17:10:03'),
 	("Moi !!! Où et à quelle heure ?", 3, 2, '2022-04-25 19:16:18');
 
-INSERT INTO `relationships` (`user_id_a`, `user_id_b`, `status`)
-VALUES
-	(1, 3, 'approved'),
-	(4, 2, 'approved'),
-	(2, 3, 'approved'),
-	(1, 2, 'pending');
 
 UPDATE `users`
 SET `status` = 'inactive'
 WHERE `first_name` = 'Laurence';
 
-CREATE TABLE IF NOT EXISTS `stats` (
-	`stat_id` INT NOT NULL AUTO_INCREMENT,
-	`nb_articles` INT DEFAULT 0,
-	`nb_comments` INT DEFAULT 0,
-	`nb_likes` INT DEFAULT 0,
-	`likes_on_articles` INT DEFAULT 0,
-	`likes_on_comments` INT DEFAULT 0,
-	`comments_on_articles` INT DEFAULT 0,
-	`nb_friends` INT DEFAULT 0,
-	`user_id` INT NOT NULL,
-	PRIMARY KEY (`stat_id`)
-) ENGINE=InnoDB;
-
-ALTER TABLE `stats`
-ADD CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE;
 
 INSERT INTO `relationships` (`user_id_a`,`user_id_b`,`status`)
 VALUES
-(1,2,'approved'),
 (1,3,'approved'),
+(1,4,'approved'),
 (3,2,'approved');
 
 
-INSERT INTO `stats` (`user_id`,`nb_articles`,`nb_friends`)
+INSERT INTO `stats` (`user_id`,`nb_articles`,`nb_friends`,`nb_comments`,`comments_on_articles`)
 VALUES
-(1,2,2),
-(2,2,2),
-(3,2,2);
-
-
-UPDATE `stats`
-SET `nb_articles` = 3
-WHERE `user_id` = 1;
+(1,1,2,0,2),
+(2,1,1,1,0),
+(3,1,2,1,1),
+(4,1,1,1,0);
