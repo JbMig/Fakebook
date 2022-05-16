2c8d0e97e1f292b959a7eb376d15c93b89ca0a32
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-05-16 16:06:44 +0200

    fix: debugging

--- php_partial/new_page.php
+++ php_partial/new_page.php
@@ -1,55 +1,39 @@
 <?php
     if($_SERVER["REQUEST_METHOD"] === "POST") {
-        if(isset($_POST["commentInput"])) {
+        if(isset($_POST["pageInput"])) {
             require_once __DIR__ . "/../database/pdo.php";
-            $text = filter_input(INPUT_POST, "commentInput");
-            $article_id = filter_input(INPUT_POST, "article_id");
+            $page_name = filter_input(INPUT_POST, "pageInput");
+            $page_description = filter_input(INPUT_POST, "descriptionInput");
             $user_id = $_SESSION["user"]["user_id"];
-            
+            // updating table pages
             $maRequete = $pdo->prepare(
-                "INSERT INTO `comments` (`content`, article_id, `user_id`)
-                VALUES(:content, :article_id, :userId)");
+                "INSERT INTO `pages` (`name`, `description`)
+                VALUES(:page_name, :page_description)");
                 $maRequete->execute([
-                    ":content" => $text,
-                    ":article_id" => $article_id,
+                    ":page_name" => $page_name,
+                    ":page_description" => $page_description
+                ]);
+			// get the new page's id
+			$maRequete = $pdo->prepare(
+                "SELECT `page_id` FROM `pages` WHERE `name` = :page_name");
+                $maRequete->execute([
+                    ":page_name" => $page_name
+                ]);
+			$page_id = $maRequete->fetch();
+
+			// updating table admins
+			$maRequete = $pdo->prepare(
+                "INSERT INTO `admins` (`page_id`, `user_id`)
+                VALUES(:pageId, :userId)");
+                $maRequete->execute([
+                    ":pageId" => $page_id,
                     ":userId" => $user_id
                 ]);
-			// updating stats of the commenter
-			$maRequete = $pdo->prepare(
-				"UPDATE `stats`
-				SET `nb_comments` = `nb_comments` + 1
-				WHERE `user_id` = :userId2;");
-				$maRequete->execute([
-					":userId2" => $user_id
-				]);
-
-
-			// getting the article's writer's user_id
-			$maRequete = $pdo->prepare(
-				"SELECT `article_id`,`user_id`
-				FROM `articles`
-				WHERE `article_id` = :article_id2;");
-				$maRequete->execute([
-					":article_id2" => $article_id
-				]);
-			$article = $maRequete->fetch(PDO::FETCH_ASSOC);
-			// updating stats of the article's writer
-			$maRequete = $pdo->prepare(
-				"UPDATE `stats`
-				SET `comments_on_articles` = `comments_on_articles` + 1
-				WHERE `user_id` = :id;");
-				$maRequete->execute([
-					":id" => $article['user_id']
-				]);
 
 
             http_response_code(302);
-            $direction = explode("/",$_SERVER["HTTP_REFERER"]);
-            if($direction[3] === "profile") {
-                header("Location: /profile");
-            } else if ($direction[3] === "timeline") {
-                header("Location: /timeline");
-            }
+            header("Location: /public_page");
+            
             exit();
         }
     }
