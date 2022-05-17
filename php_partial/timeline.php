d05ec90703a083680a4fbb21ea591195d4a45bc4
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-05-17 21:26:28 +0200

    refactor: various request tries

--- php_partial/timeline.php
+++ php_partial/timeline.php
@@ -8,15 +8,6 @@ require_once "../database/pdo.php";
 $user_id = $_SESSION["user"]["user_id"];
 $user_name = $_SESSION["user"]["first_name"] . " " . $_SESSION["user"]["last_name"];
 
-// getting all articles
-$maRequete = $pdo->prepare("SELECT * FROM `articles` ORDER BY `date` DESC"); // add condition for relationship
-    $maRequete->execute();
-    $articles = $maRequete->fetchAll(PDO::FETCH_ASSOC);
-
-// getting all active accounts
-$maRequete = $pdo->prepare("SELECT `user_id`, `profil_picture`, `first_name`, `last_name`, `status` FROM `users` WHERE `status` = 'active'"); // add condition for relationship
-    $maRequete->execute();
-    $profiles = $maRequete->fetchAll(PDO::FETCH_ASSOC);
 
 // getting likes
 $maRequete = $pdo->prepare("SELECT * FROM `likes` WHERE `user_id` = :userId");
@@ -26,15 +17,6 @@ $maRequete = $pdo->prepare("SELECT * FROM `likes` WHERE `user_id` = :userId");
     $user_likes = $maRequete->fetchAll(PDO::FETCH_ASSOC);
     $like = "like";
 
-// looking for the user's friends
-$maRequete = $pdo->prepare("SELECT `user_id_a`, `user_id_b`, `status`, `blocked` FROM `relationships` WHERE (`user_id_b` = :userId OR `user_id_a` = :userId) AND `status`='approved';");
-	$maRequete->execute([
-		":userId" => $_SESSION["user"]["user_id"]
-	]);
-$profile_friends = $maRequete->fetchAll(PDO::FETCH_ASSOC);
-
-
-
 
 // getting all followed pages (cette requête fonctionne)
 $maRequete = $pdo->prepare("SELECT `page_id`, `name`, `picture` FROM `pages` WHERE `page_id` IN (SELECT `page_id` FROM `followers` WHERE `user_id` = :userId);");
@@ -43,8 +25,45 @@ $maRequete = $pdo->prepare("SELECT `page_id`, `name`, `picture` FROM `pages` WHE
 	]);
 $followed_pages = $maRequete->fetchAll(PDO::FETCH_ASSOC);
 
-var_dump($followed_pages);
+// $maRequete = $pdo->prepare("SELECT `page_id` FROM `pages` WHERE `page_id` IN (SELECT `page_id` FROM `followers` WHERE `user_id` = :userId);");
+// 	$maRequete->execute([
+// 		":userId" => $user_id
+// 	]);
+// $followed_pages_id = $maRequete->fetchAll(PDO::FETCH_ASSOC);
 
+$followed_pages_id = array();
+foreach ($followed_pages as $followed_page) {
+	array_push($followed_pages_id, $followed_page["page_id"]);
+}
+
+// var_dump($followed_pages_id);
+// exit();
+
+$maRequete = $pdo->prepare("SELECT `user_id_a` FROM `relationships` WHERE `user_id_b` = :userId AND `status`='approved';");
+	$maRequete->execute([
+		":userId" => $user_id
+	]);
+$friends_a = $maRequete->fetchAll(PDO::FETCH_ASSOC);
+
+$maRequete = $pdo->prepare("SELECT `user_id_b` FROM `relationships` WHERE `user_id_a` = :userId AND `status`='approved';");
+	$maRequete->execute([
+		":userId" => $user_id
+	]);
+$friends_b = $maRequete->fetchAll(PDO::FETCH_ASSOC);
+
+$friends = array_merge($friends_a, $friends_b);
+// jusqu'ici ça marche
+// var_dump($friends);
+// exit();
+
+$maRequete = $pdo->prepare("SELECT * from `articles` WHERE `user_id` IN :friends OR `page_id` IN :followed_pages_id;");
+	$maRequete->execute([
+		":friends" => $friends,
+		":followed_pages_id" => $followed_pages_id
+	]);
+$articles = $maRequete->fetchAll(PDO::FETCH_ASSOC);
+var_dump($articles);
+exit();
 
 
 
