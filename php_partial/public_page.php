f7d37c37cad72e192ac1060176b9c726e6e2ea1f
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-05-17 14:08:21 +0200

    fix: debugging

--- php_partial/public_page.php
+++ php_partial/public_page.php
@@ -44,7 +44,7 @@ $followers = $maRequete->fetchAll(PDO::FETCH_ASSOC);
 $nb_followers = COUNT($followers);
 
 
-$accounts = [];
+$accounts = array();
 
 foreach ($followers as $follower) {
 	if ($follower['user_id'] === $_SESSION["user"]["user_id"]) {
@@ -53,7 +53,7 @@ foreach ($followers as $follower) {
 			$maRequete->execute([
 				":Id" => $follower['user_id']
 			]);
-		$accounts_push($maRequete->fetch());
+		array_push($accounts, $maRequete->fetch());
 	}
 	else {
 		$is_follower = FALSE;
