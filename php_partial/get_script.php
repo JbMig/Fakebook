451c7973b3d4fa811e3bb1bf734d8ef6a6ca711a
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-05-17 11:10:00 +0200

    feat: modify articles

--- php_partial/get_script.php
+++ php_partial/get_script.php
@@ -6,6 +6,8 @@
     <script src="script/timeline_script.js?<?php echo time(); ?>"></script>
 <?php elseif($uri === "/profile") :?>
     <script src="script/profile_script.js?<?php echo time(); ?>"></script>
+<?php elseif($uri === "/public_page") :?>
+    <script src="script/public_page_script.js?<?php echo time(); ?>"></script>
 <?php elseif($uri === "/new_chat") :?>
     <script src="script/new_chat_script.js?<?php echo time(); ?>"></script>
 <?php endif; ?>
