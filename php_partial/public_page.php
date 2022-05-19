7845c4ee7e91896778713b9de968fcea7f5f1820
Loïc Jin <loicjin@MacBook-Pro-de-Loic.local>
2022-05-19 16:00:41 +0200

    test

--- php_partial/public_page.php
+++ php_partial/public_page.php
@@ -39,7 +39,7 @@ $articles = $maRequete->fetchAll(PDO::FETCH_ASSOC);
 // getting the page's stats
 $nb_articles = Count($articles);
 
-$maRequete = $pdo->prepare("SELECT `follower_id`, `user_id` FROM `followers` WHERE `page_id` = :pageId;");
+$maRequete = $pdo->prepare("SELECT `follower_id`, `user_id` FROM `followers` WHERE `page_id` = :pageId AND `banned` = 'no';");
 	$maRequete->execute([
 		":pageId" => $page_id
 	]);
@@ -60,7 +60,6 @@ if (COUNT($user_is_follower)>0){
 }
 
 $accounts = array();
-
 foreach ($followers as $follower) {
 	$maRequete = $pdo->prepare("SELECT `user_id`, `first_name`, `last_name`, `profil_picture` FROM `users` WHERE `user_id` = :Id;");
 		$maRequete->execute([
@@ -69,9 +68,6 @@ foreach ($followers as $follower) {
 		$maRequete->setFetchMode(PDO::FETCH_ASSOC);
 	array_push($accounts, $maRequete->fetch());
 }
-
-
-
 
 
 // getting the page's admins
@@ -90,6 +86,41 @@ foreach ($admins as $admin) {
 		$is_admin = FALSE;
 	}
 }
+
+
+// getting those who were banned from the page (and checking whether the user himself is banned)
+
+$maRequete = $pdo->prepare("SELECT `follower_id`, `user_id` FROM `followers` WHERE `page_id` = :pageId AND `banned` = 'yes';");
+	$maRequete->execute([
+		":pageId" => $page_id
+	]);
+$banned_persons = $maRequete->fetchAll(PDO::FETCH_ASSOC);
+$nb_banned_persons = COUNT($banned_persons);
+
+
+$maRequete = $pdo->prepare("SELECT `user_id` FROM `followers` WHERE `page_id` = :pageId AND `user_id` = :userId AND `banned` = 'yes';");
+	$maRequete->execute([
+		":pageId" => $page_id,
+		":userId" => $user_id
+	]);
+$user_is_banned = $maRequete->fetchAll(PDO::FETCH_ASSOC);
+if (COUNT($user_is_banned)>0){
+	$is_banned = TRUE;
+} else {
+	$is_banned = FALSE;
+}
+
+$banned_accounts = array();
+foreach ($banned_persons as $banned_person) {
+	$maRequete = $pdo->prepare("SELECT `user_id`, `first_name`, `last_name`, `profil_picture` FROM `users` WHERE `user_id` = :Id;");
+		$maRequete->execute([
+			":Id" => $banned_person['user_id']
+		]);
+		$maRequete->setFetchMode(PDO::FETCH_ASSOC);
+	array_push($banned_accounts, $maRequete->fetch());
+}
+
+
 // checking whether we're friends with the person
 $profile_id = filter_input(INPUT_POST, "profil_id");
 
