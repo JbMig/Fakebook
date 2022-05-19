cc621e4779368573212e98f9d45c6423f27ff87b
Loïc Jin <loicjin@MacBook-Pro-de-Loic.local>
2022-05-19 15:18:49 +0200

    style : sign up done

--- html_partial/sign_up.php
+++ html_partial/sign_up.php
@@ -1,4 +1,36 @@
-<div class="container col-md-4 d-grid gap-2  border border-primary rounded" style="margin-top : 5%; margin-bottom: 5%;">
+<div class="block2">
+    <form method="POST">
+    <div class="containerSignUp">
+        <div class="contSign">
+                <label class="test" for="firstName">First Name :</label>
+                <input class="inputSign" type="text" id="firstName" name="firstName" placeholder="">
+        </div>
+        <div class="contSign">
+                <label class="test" for="lastName">Last Name :</label>
+                <input class="inputSign" type="text" id="lastName" name="lastName" placeholder="">
+        </div>
+        <div class="contSign">
+                <label class="test" for="email">Email :</label>
+                <input class="inputSign" type="email" id="email" name="email" placeholder="">
+        </div>
+        <div class="contSign">
+            <label class="test" for="password">Password :</label>
+            <input class="inputSign" type="password" id="password" name="password" placeholder="">
+        </div>
+        <div class="contSign">
+                <label class="test" for="confirmPassword">Confirm password :</label>
+                <input class="inputSign" type="password" id="confirmPassword" name="confirmPassword" placeholder="">
+        </div>
+            <div class="signButton">
+                <input class="validate" type="submit" id="valider" value="Valider"/>
+                <button type="button" class="validate"><a class="sign" id="inscrire" href="/login">Login</a></button>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+<!-- <div class="container col-md-4 d-grid gap-2  border border-primary rounded" style="margin-top : 5%; margin-bottom: 5%;">
     <form method="POST">
         <label class="col-md-4 mt-3 mx-5 " for="firstName">First Name :</label>
         <input class="col-md-8 mx-5" type="text" id="firstName" name="firstName" placeholder="">
@@ -13,4 +45,4 @@
         <input class="col-md-4 mt-4 mx-5 bg-primary" type="submit" id="valider" value="Valider"/>
     </form>
     <a class="col-md-4 mb-3 mt-2 mx-5" id="inscrire" href="/login">Login</a>
-</div>
+</div> -->
