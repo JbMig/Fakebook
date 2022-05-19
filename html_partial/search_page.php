b26855254694bfdfd91a1bb6615e1bb59d893dad
lightvic <v_guillerd@hetic.eu>
2022-05-19 10:03:17 +0200

    refactor : test for working

--- html_partial/search_page.php
+++ html_partial/search_page.php
@@ -12,7 +12,7 @@ foreach ($pages as $page){
             if(in_array($name_page,$memory) == false){
             array_push($memory,$name_page);?>
             <form id="goToProfile" action="/public_page" method="post">
-            <input type="hidden" name="profil_id" value="<?= $_SESSION["page"]["page_id"]; ?>" />
+            <input type="hidden" name="profil_id" value="<?= $page["page_id"]; ?>" />
                 <button type="submit" id="profil_picture" style="background: white; border:0; padding:5px;">
                     <img src="img_pages_groups/<?=  $name["picture"] ?>" alt="" width="40px">
                 </button>
@@ -34,7 +34,7 @@ foreach ($names as $name):
     if (in_array($name_page,$memory) == false){
         ?>
         <form id="goToProfile" action="/public_page" method="post">
-        <input type="hidden" name="profil_id" value="<?= $_SESSION["page"]["page_id"]; ?>" />
+        <input type="hidden" name="profil_id" value="<?= $name["page_id"]; ?>" />
         <button type="submit" id="profil_picture" style="background: white; border:0; padding:5px;">
                     <img src="img_pages_groups/<?=  $name["picture"] ?>" alt="" width="40px">
                 </button>
