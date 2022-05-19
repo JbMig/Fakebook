c481f49f263666842a2c4f130ab6e295150d7721
Loïc Jin <loicjin@MacBook-Pro-de-Loic.local>
2022-05-19 11:45:18 +0200

    refactor : style searching

--- html_partial/login.php
+++ html_partial/login.php
@@ -1,6 +1,26 @@
 
 <div class="block1">
     <form class="form" action="/login" method="post">
+        <div class="container">
+        <div id="tripleLogin"> 
+            <h1 class="login">BIENVENUE</h1>
+            <h1 class="login">SUR
+            <h1 class="login">FAKEBOOK</h1>
+        </div>
+            <label class="mail" for="email">Adresse mail :</label>
+            <input class="inputSign" type="email" id="email" name="mail" placeholder="">
+            <label class="mdp" for="password">Mot de passe :</label>
+            <input class="inputSign" type="password" id="password" name="mdp" placeholder="">
+            <button class="validate" type="submit" id="valider" name="valider" >Valider</button>
+            <a class="sign" id="inscrire" href="/sign_up">S'inscrire</a>
+        </div>
+    </form>
+   
+</div>
+
+
+<!-- <div class="block1">
+    <form class="form" action="/login" method="post">
         <div class="container col-md-3 d-grid gap-2 bg-light border border-primary rounded" style="margin-top : 10%">
             <h1 class="mx-auto">BIENVENUE</h1>
             <h1 class="mx-auto">SUR</h1>
@@ -12,4 +32,4 @@
             <button class=" p-1 col-md-4 bg-primary mt-3" type="submit" id="valider" name="valider" >Valider</button>
         </form>
     <a class="my-3" id="inscrire" href="/sign_up">S'inscrire</a>
-</div>
+</div> -->
