b81afe11c1603505df54c96767400475a3509e97
Loïc Jin <loicjin@MacBook-Pro-de-Loic.local>
2022-05-20 22:08:28 +0200

    fix : redirection of delete article and comment

--- php_partial/delete_article.php
+++ php_partial/delete_article.php
@@ -34,6 +34,9 @@
                     } else if ($direction[3] === "public_page") {
                         header("Location: /public_page");
                     }
+                      else if ($direction[3] === "group") {
+                        header("Location: /group");
+                    }
                     exit();
                 }
             } else {
